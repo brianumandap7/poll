dfbdab9c7f335930ba6ff577bf5c6a7348824455
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2022-05-13 10:19:11 +0800

    fixed

--- poll/views.py
+++ poll/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect, HttpResponse
 from django.contrib.auth.models import User
+from .models import Question
 
 # Create your views here.
 
 
 def poll(request):
     query = {
+    	'question': Question.objects.all(),
 
     }
     return render(request, 'poll/poll.html', query)
 
-def detail(request):
+def detail(request, question_id = 0):
     query = {
-
+    	'question_id': question_id,
+    	'detail': Question.objects.filter(id = question_id),
     }
     return render(request, 'poll/detail.html', query)
 

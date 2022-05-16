0bd6d7db32c8681fc500af671bea640ae7e2cc7a
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2022-05-16 09:12:15 +0800

    t4

--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect, HttpResponse
 from django.contrib.auth.models import User
-from .models import Question
+from .models import Question, Choice
+from django.db.models import F
 
 # Create your views here.
 
@@ -16,7 +17,15 @@ def detail(request, question_id = 0):
     query = {
     	'question_id': question_id,
     	'detail': Question.objects.filter(id = question_id),
+        'cdetail': Choice.objects.filter(question_id = question_id),
     }
+
+    if request.method == "POST":
+        if request.POST.get('vt1'):
+            Choice.objects.filter(id = 1).update(votes=F('votes')+1)
+        if request.POST.get('vt4'):
+            Choice.objects.filter(id = 4).update(votes=F('votes')+1)
+        return HttpResponseRedirect('results/')
     return render(request, 'poll/detail.html', query)
 
 def vote(request):
@@ -25,9 +34,11 @@ def vote(request):
     }
     return render(request, 'poll/poll.html', query)
 
-def results(request):
+def results(request, question_id = 0):
     query = {
-
+        'question_id': question_id,
+        'detail': Question.objects.filter(id = question_id),
+        'cdetail': Choice.objects.filter(question_id = question_id),
     }
     return render(request, 'poll/results.html', query)
 
